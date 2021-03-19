546a2369f171d9149f8356b0f1c7a98130450c09
Nils Brugger <nbrugger@student.tgm.ac.at>
2021-03-19 16:22:48 +0100

    docs: comment bitbang

--- src/bit_bang.cpp
+++ src/bit_bang.cpp
@@ -6,16 +6,20 @@
 #include <climits>
 #include "includes/bit_bang.h"
 
+
 void shift(uint *number, bool bit) {
     *number = *number << 1;
     *number = *number | (bit ? 1 : 0);
 }
 
+
+
 void print_binary(uint number) {
-    for (int i = 0; i < sizeof(number)*CHAR_BIT; ++i) {
+    for (int i = 0; i < sizeof(typeof(number))*2; ++i) {
         printf("%u",number>>i & 1);
     }
 }
+
 
 void binary_to_int(bool *buf,int bufsize, uint *number) {
     for(int i = 0;i<bufsize;i++){
--- src/includes/bit_bang.h
+++ src/includes/bit_bang.h
@@ -3,8 +3,26 @@
 //
 #ifndef TEST_PROJECT_BIT_BANG_H
 #define TEST_PROJECT_BIT_BANG_H
-
+/**
+ * Shifts a bit into the number. Works like a shift register.
+ *
+ * shift(0110,1) will result in the number that was 0110 bevore to be 10110 with 1 being the LSB
+ * @param number the number to use as shift register
+ * @param bit the bit to shift
+ */
 void shift(uint* number, bool bit);
+
+/**
+ * Uses printf to print a binary representation of a number
+ * @param number the number to print in binary
+ */
 void print_binary(uint number);
+
+/**
+ * Converts a series of bits (aka bool[]) to a int
+ * @param buf the bits to convert
+ * @param bufsize the number of bits
+ * @param number the number to save the result into
+ */
 void binary_to_int(bool buf[],int bufsize,uint* number);
 #endif //TEST_PROJECT_BIT_BANG_H
